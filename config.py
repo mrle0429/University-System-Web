7dd343e0c4e9374bf137174bac5a3757fab4079d
zbh <bohan.zhang@ucdconnect.ie>
2024-10-22 22:12:57 +0800

    Change database to MySQL and add config

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 
+
 class Config:
     SECRET_KEY = 'secretary'
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance/app.db')
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/app.db'
+
+    # Update this line to use MySQL with remote credentials
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:OOD20241022@39.105.0.202/webapp'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
