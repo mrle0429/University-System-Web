de7130cc72e47e9f4b491d1c552205f479fd2be9
zbh <bohan.zhang@ucdconnect.ie>
2024-10-30 20:01:00 +0800

    Add first version of forum

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,1 +1,14 @@
 console.log("Welcome to the Flask Project!");
+
+function toggleReplies() {
+    const repliesSection = document.querySelector(".replies");
+    const toggleButton = document.querySelector(".toggle-replies");
+
+    if (repliesSection.style.display === "none") {
+        repliesSection.style.display = "block";
+        toggleButton.textContent = "Hide Replies";
+    } else {
+        repliesSection.style.display = "none";
+        toggleButton.textContent = "Show Replies";
+    }
+}
